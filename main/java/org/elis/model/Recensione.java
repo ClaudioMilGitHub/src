1261099c155866a9b094baf0051f266dfbcea0e1
Medoors <simone.medori90@gmail.com>
2024-10-10 14:48:49 +0200

    Annotazioni Recensioni + aggiunta ID

--- main/java/org/elis/model/Recensione.java
+++ main/java/org/elis/model/Recensione.java
@@ -1,12 +1,34 @@
 package org.elis.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+import jakarta.persistence.ManyToOne;
+
+
 public class Recensione {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "is_recommended")
 	private boolean isRecommended;
+	@Column(name = "testo")
 	private String testo;
+	
+	
+	@ManyToOne
 	private Utente utente;
+	@ManyToOne
 	private Gioco gioco;
 	
-	public Recensione(Boolean isRecommended, String testo) {
+	
+	
+	public Recensione(Long id, Boolean isRecommended, String testo) {
+		this.id= id;
 		this.isRecommended = isRecommended;
 		this.testo = testo;
 	}
